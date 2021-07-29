d52dd66bccd28d46571fc2f212fd7dc6ac76e7b7
lixuan <lixuan@iyunbao.com>
2021-07-29 18:59:56 +0800

    *update cors config

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express();
 // 解决跨域问题
 const allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://dev.iyunbao.com:8081"); //自定义中间件，设置跨域需要的响应头。
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "x-custom, Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, x-custom, Content-Type");
   res.header("Access-Control-Allow-Credentials", "true"); //和客户端对应，必须设置以后，才能接收cookie.
   next();
 };
